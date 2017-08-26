0a4c410521337d1f16fb75b207881e6377e5dac6
krelly <itwillnotbeasy@gmail.com>
2017-08-26 15:00:43 +0300

    Code cleanup

--- lib/steam/market.rb
+++ lib/steam/market.rb
@@ -1,4 +1,5 @@
 require 'date'
+
 module Steam
   class Market
     def initialize(cookies:, currency: Currency::USD, appid: 730)
@@ -9,49 +10,42 @@ module Steam
     end
 
     def price_overview(item_name)
-      res = get("priceoverview", {
-              country: @country,
-              currency: @currency,
-              appid: @appid,
-              market_hash_name: item_name
-      })
-
+      get('priceoverview', market_hash_name: item_name)
     end
 
     def price_history(item_name, period = :daily)
-      res = get("pricehistory", {
-              country: @country,
-              currency: @currency,
-              appid: @appid,
-              market_hash_name: item_name
-      })
+      res = get('pricehistory', market_hash_name: item_name)
       prices = res['prices'].map do |(date, avg_price, volume)|
         {
-                # "Feb 25 2017 09: +0", time zone (+0) is always zero ????
-                date: DateTime.strptime(date,"%b %d %Y %H"),
-                price: avg_price,
-                volume: volume.to_i
+          # "Feb 25 2017 09: +0", time zone (+0) is always zero ????
+          date: DateTime.strptime(date, '%b %d %Y %H'),
+          price: avg_price,
+          volume: volume.to_i
         }
       end
-      # e = prices.group_by{|e| e[:date].to_date}.map { |k,v| v}.last
+
       if period == :daily
-        prices.group_by{|e| e[:date].to_date}.map {|date, sales| weighted_mean(sales).merge(date:date)}
+        prices.group_by { |e| e[:date].to_date }.map { |date, sales| weighted_mean(sales).merge(date: date) }
       else
         weighted_mean(prices)
       end
     end
 
-    def search()
-
-    end
+    def search; end
 
     def get(url, params = {})
-      res = JSON.parse @market[url].get(params: params)
+      default_params = {
+        country: @country,
+        currency: @currency,
+        appid: @appid
+      }
+      res = JSON.parse @market[url].get(params: default_params.merge(params))
       raise InvalidResponse unless res['success']
       res
     end
 
     private
+
     def weighted_mean(prices)
       a = 0
       b = 0
@@ -59,8 +53,7 @@ module Steam
         a += e[:price] * e[:volume]
         b += e[:volume]
       end
-      {volume: b, price: a/b}
+      { volume: b, price: a / b }
     end
   end
-
 end
