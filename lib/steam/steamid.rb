ea868c91ef889ea25858019545180eba175d1a32
krelly <itwillnotbeasy@gmail.com>
2019-02-18 15:16:32 +0200

    Add docs, remove redundant self

--- lib/steam/steamid.rb
+++ lib/steam/steamid.rb
@@ -1,17 +1,25 @@
 module Steam
+  # A SteamID is a unique identifier used to identify a Steam account.
+  # It is also used to refer to a user's Steam Community profile page.
   class SteamID
+    # TODO: this is actually STEAM_0:0:0, and should be named accordingly
     STEAMID_CONVERSION_BASE = 76561197960265728
+
+    # TODO: constructor for STEAM_X:Y:Z, where
+    #   X represents the "Universe" the steam account belongs to.
+    #   Y is part of the ID number for the account. Y is either 0 or 1.
+    #   Z is the ID number for the account (the "account number").
 
     def initialize(steamid64)
       @steamid64 = Integer(steamid64)
     end
 
     def self.from_vanity_url(web_api, vanity_url)
-      self.new(web_api.get('ISteamUser', 'ResolveVanityURL', vanityurl: vanity_url)[:steamid])
+      new(web_api.get('ISteamUser', 'ResolveVanityURL', vanityurl: vanity_url)[:steamid])
     end
 
     def self.from_steamid32(steamid32)
-      self.new(to_steamid64(steamid32))
+      new(to_steamid64(steamid32))
     end
 
     def as_64
@@ -30,5 +38,11 @@ module Steam
     def ==(other)
       @steamid64 == other.as_64
     end
+
+    def self.to_steamid64(id32)
+      id32 + STEAMID_CONVERSION_BASE
+    end
+
+    private_class_method :to_steamid64
   end
 end
